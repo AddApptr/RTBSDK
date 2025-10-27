d3a6f6bcaad64d195808bd8a0cb685f734de1196
Mahmoud Amer <mahmoud.amer@addapptr.com>
2025-10-27 10:20:26 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "RTBSPM", targets: ["RTBSDK", "RTBOMSDK"]),
+        .library(name: "RTBSDK", targets: ["RTBSDK", "RTBOMSDK"]),
         .library(name: "RTB_GraviteRTBAppLovinMediationAdapter", targets: ["RTB_GraviteRTBAppLovinMediationAdapter"]),
         .library(name: "RTB_GraviteRTBAdMobMediationAdapter", targets: ["RTB_GraviteRTBAdMobMediationAdapter"]),
     ],
